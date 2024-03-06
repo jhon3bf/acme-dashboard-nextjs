0ba837d4a0a4964368a075a899614f33ea089bc9
sagit <sagitgame@gmail.com>
2024-03-06 07:29:53 -0500

    acme project

---
+++ app/lib/definitions.ts
@@ -0,0 +1,26 @@
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Customer = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+};
+
+export type Invoice = {
+  id: string;
+  customer_id: string;
+  amount: number;
+  date: string;
+  status: 'pendings' | 'paid';
+};
+
+export type Revenue = {
+  month: string;
+  revenue: number;
+};
